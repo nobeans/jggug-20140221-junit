51734e756fc638a5b41bd6e4d8023495ffdf9e65
Yasuharu Nakano <ynak@jggug.org>
2014-02-21 20:13:54 +0900

    [from now] 2014/02/21 20:13:54
     src/main/groovy/jggug/JGGUGRunner.groovy | 35 ++++++++++++++++++++++++--------
     1 file changed, 26 insertions(+), 9 deletions(-)

--- src/main/groovy/jggug/JGGUGRunner.groovy
+++ src/main/groovy/jggug/JGGUGRunner.groovy
@@ -1,31 +1,31 @@
 package jggug
 
 import org.junit.runner.Description
+import org.junit.runner.Result
 import org.junit.runner.Runner
 import org.junit.runner.notification.RunNotifier
 
 class JGGUGRunner extends Runner {
 
     private Class klass
+    private Description desc
 
     JGGUGRunner(Class<?> klass) {
         this.klass = klass
         println "Constructor: $klass"
-    }
 
-    int count = 0
-
-    @Override
-    Description getDescription() {
-        println klass.name
-        def desc = Description.createTestDescription(klass.name, "B")
+        desc = Description.createTestDescription(klass.name, "")
         klass.declaredMethods.each { method ->
             if (method.returnType != Object) return
             if (method.parameterTypes.size() > 0) return
             if (method.name =~ /super\$.*/) return
             desc.addChild Description.createTestDescription(klass.name, method.name)
-            count++
+            println "Found test method: ${method.name}"
         }
+    }
+
+    @Override
+    Description getDescription() {
         return desc
     }
 
@@ -35,12 +35,29 @@ class JGGUGRunner extends Runner {
 
         description.children.each { child ->
             notifier.fireTestStarted(child)
+
+            def instance = klass.newInstance()
+
+            println ">" * 10
+            println "Running ${child.displayName}"
+
+            def result = new Result()
+            def listener = result.createListener()
+
+            try {
+                instance.invokeMethod(child.methodName, null)
+
+                listener.testFinished(child)
+                notifier.fireTestRunFinished(result)
+            } catch (AssertionError e) {
+                listener.testFailure(null)
+                notifier.fireTestFailure(result)
+            }
         }
 
         //def result = new Result()
         //def listener = result.createListener()
         //listener.testRunFinished(null)
         //notifier.fireTestRunFinished(result)
-        println "run"
     }
 }
